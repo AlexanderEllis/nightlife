cff5d366af3a7606aeb7b27dfbc5ce987714b43d
Alexander Ellis <ellisonjtk@gmail.com>
2017-04-23 12:39:24 -0400

    added acceptance tests

---
+++ tests/acceptance/list-bars-test.js
@@ -0,0 +1,31 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'nightlife-client/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list bars');
+
+test('should show bars for user\'s location as the home page', function(assert) {
+});
+
+test('should link to information about the app', function(assert) {
+});
+
+test('should list bars', function(assert) {
+});
+
+test('should filter the list of bars by name', function(assert) {
+});
+
+test('should search bars by location', function(assert) {
+});
+
+test('should show details for a selected bar', function(assert) {
+});
+
+test('As an authenticated user, I can add myself to a bar to indicate I am going there tonight.', function(assert) {
+});
+
+test('As an authenticated user, I can remove myself from a bar if I no longer want to go there.', function(assert) {
+});
+
+test('As an unauthenticated user, when I login I should not have to search again.', function(assert) {
+});
