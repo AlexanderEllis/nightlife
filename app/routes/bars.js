99a584d30b886dc17536f7d89c41c57e5b4186ac
Alexander Ellis <ellisonjtk@gmail.com>
2017-04-25 20:58:44 -0400

    added bars model

--- app/routes/bars.js
+++ app/routes/bars.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [{
+      id: 'sports-bar',
+      title: 'Sports Bar',
+      location: 'Brighton',
+      going: 0,
+      description: 'This is a great bar to catch a game at.',
+      image: 'sports-bar.jpg'
+    }, {
+      id: 'dive-bar',
+      title: 'Dive Bar',
+      location: 'Allston',
+      going: 0,
+      description: 'This is a great bar to have a cheap beer.',
+      image: 'dive-bar.jpg'
+    }, {
+      id: 'wine-bar',
+      title: 'Wine Bar',
+      location: 'Brookline',
+      going: 0,
+      description: 'This is a great bar to have wine at.',
+      image: 'wine-bar.jpg'
+    }];
+  }
 });
