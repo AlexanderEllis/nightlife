75ac49a745642ae6f0a65d7bc12c80d6cf17ea81
Alexander Ellis <ellisonjtk@gmail.com>
2017-04-26 17:17:16 -0400

    bar-listing component changes

--- app/components/bar-listing.js
+++ app/components/bar-listing.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    isWide: false, // Pass this key/value to the component template, which we can then use with hbs logic
+    actions: { // We can define all of the actions accessible by the component template
+        toggleImageSize() {
+            this.toggleProperty('isWide');
+        }
+    }
 });
