33b9612e881c07ec4c20ab0d0a13cbf9105e4dac
Alexander Ellis <ellisonjtk@gmail.com>
2017-04-27 08:48:46 -0400

    added integration test for bar listing

--- tests/integration/components/bar-listing-test.js
+++ tests/integration/components/bar-listing-test.js
@@ -1,25 +1,35 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+let bar = Ember.Object.create({
+    id: 'test-bar',
+    title: 'test-title',
+    location: 'test-city',
+    going: 0,
+    description: 'test-description',
+    image: 'fake.png'
+});
 
 moduleForComponent('bar-listing', 'Integration | Component | bar listing', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('should display rental details', function(assert) {
+  this.set('barObj', bar);
+  this.render(hbs`{{bar-listing bar=barObj}}`);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.equal(this.$('.listing h3').text(), 'test-title', 'Title: test-title');
+  assert.equal(this.$('.listing .location').text().trim(), 'Location: test-city');
+});
 
-  this.render(hbs`{{bar-listing}}`);
+test('should toggle wide class on click', function(assert) {
+  this.set('barObj', bar);
+  this.render(hbs`{{bar-listing bar=barObj}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#bar-listing}}
-      template block text
-    {{/bar-listing}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.image.wide').length, 0, 'initially rendered without wide class');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 1, 'Wide once clicked once');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 0, 'Back to normal width without wide class');
 });
